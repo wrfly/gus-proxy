0039b146d37aeabb9695e9d707fa42b9fc4861c6
wrfly <mr.wrfly@gmail.com>
2018-08-08 17:33:45 +0800

    refactor: run

--- main.go
+++ main.go
@@ -2,21 +2,87 @@ package main
 
 import (
 	"fmt"
+	// go  pprof ...
+	_ "net/http/pprof"
 	"os"
 
-	"github.com/wrfly/gus-proxy/cmds"
 	"gopkg.in/urfave/cli.v2"
+
+	"github.com/wrfly/gus-proxy/config"
 )
 
+const helpTemplate = `NAME:
+    {{.Name}} - {{.Usage}}
+{{if len .Authors}}
+AUTHOR:
+    {{range .Authors}}{{ . }}{{end}}
+{{end}}{{if .Version}}
+VERSION:
+    {{.Version}}
+{{end}}{{if .Commands}}
+OPTIONS:
+{{range .VisibleFlags}}    {{.}}
+{{end}}{{end}}`
+
 func main() {
+	conf := &config.Config{}
+
 	app := cli.App{
 		Name:  "gus-proxy",
-		Usage: "An apple a day, keep the doctor away.",
+		Usage: "Change proxy for every request",
 		Version: fmt.Sprintf("version: %s\tcommit: %s\tdate: %s",
 			Version, CommitID, BuildAt),
 		Authors: author,
-		Commands: []*cli.Command{
-			cmds.Run(),
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "file",
+				Aliases:     []string{"f"},
+				Value:       "proxyhosts.txt",
+				Usage:       "proxy file path, filepath or URL",
+				Destination: &conf.ProxyFilePath,
+			},
+			&cli.BoolFlag{
+				Name:        "debug",
+				Aliases:     []string{"d"},
+				Usage:       "debug mode",
+				Destination: &conf.Debug,
+			},
+			&cli.StringFlag{
+				Name:        "schduler",
+				Aliases:     []string{"s"},
+				Value:       "round-robin",
+				Usage:       "schduler: round-robin|ping|random",
+				Destination: &conf.Scheduler,
+			},
+			&cli.StringFlag{
+				Name:        "listen",
+				Aliases:     []string{"l"},
+				Value:       "8080",
+				Usage:       "port to bind",
+				Destination: &conf.ListenPort,
+			},
+			&cli.StringFlag{
+				Name:        "debug-port",
+				Value:       "8081",
+				Usage:       "port for pprof debug",
+				Destination: &conf.DebugPort,
+			},
+			&cli.StringFlag{
+				Name:        "ua",
+				Value:       "",
+				Usage:       "specific UA, random UA if empty",
+				Destination: &conf.UA,
+			},
+			&cli.IntFlag{
+				Name:        "update",
+				Value:       30,
+				Usage:       "proxies update interval(second)",
+				Destination: &conf.ProxyUpdateInterval,
+			},
+		},
+		CustomAppHelpTemplate: helpTemplate,
+		Action: func(c *cli.Context) error {
+			return runGus(conf)
 		},
 	}
 	app.Run(os.Args)
---
+++ run.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	
+	"github.com/wrfly/gus-proxy/config"
+	"github.com/wrfly/gus-proxy/db"
+	"github.com/wrfly/gus-proxy/round"
+)
+
+func runGus(conf *config.Config) error {
+	if conf.Debug {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+	logrus.Info("Gus is starting...")
+
+	if err := conf.Validate(); err != nil {
+		logrus.Fatalf("Verify config error: %s", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
+
+	// update proxy status
+	upChan := make(chan interface{})
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		logrus.Info("Updating proxyies...")
+		conf.UpdateProxies()
+		upChan <- true
+		close(upChan)
+
+		tk := time.NewTicker(time.Second * time.Duration(conf.ProxyUpdateInterval))
+		defer tk.Stop()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-tk.C:
+				conf.UpdateProxies()
+			}
+		}
+	}()
+	<-upChan
+
+	// handle signals
+	logrus.Debug("handle sigs")
+	sigStop := make(chan os.Signal)
+	signal.Notify(sigStop, syscall.SIGINT, syscall.SIGTERM)
+	sigKill := make(chan os.Signal)
+	signal.Notify(sigKill, os.Kill)
+
+	// init db
+	logrus.Debug("init dns db")
+	dnsDB, err := db.New()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer dnsDB.Close()
+
+	go func() {
+		if !conf.Debug {
+			return
+		}
+		addr := fmt.Sprintf(":%s", conf.DebugPort)
+		logrus.Infof("Debug is running at %s", addr)
+		http.ListenAndServe(addr, nil)
+	}()
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf("0.0.0.0:%s", conf.ListenPort),
+		Handler: round.New(conf, dnsDB),
+	}
+	go func() {
+		wg.Add(1)
+		defer wg.Done()
+		logrus.Infof("Bind port [%s] and run...", conf.ListenPort)
+		err := srv.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			logrus.Error(err)
+		}
+	}()
+
+	select {
+	case <-sigStop:
+		logrus.Info("About to stop")
+		cancel()
+		srvCtx, srvCancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer srvCancel()
+		srv.Shutdown(srvCtx)
+		quit := make(chan struct{})
+		go func() {
+			wg.Wait()
+			quit <- struct{}{}
+		}()
+		defer close(quit)
+
+		select {
+		case <-sigStop:
+			srvCancel()
+			logrus.Warn("Force quit!")
+		case <-quit:
+			logrus.Info("Quit")
+		}
+	case <-sigKill:
+		cancel()
+		srv.Close()
+	}
+
+	logrus.Info("Gus stopped")
+	return nil
+}
